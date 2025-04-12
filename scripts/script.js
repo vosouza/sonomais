d1ee9daf4f6908d49767bdbc9db05192c91f2f8d
vinicius <cpudvini@gmail.com>
2025-04-12 14:56:16 -0300

    composer e arquivo htacess

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const nav = document.getElementById('navLinks');
-    const icon = document.getElementById('menuIcon');
-    const body = document.body;
-
-    nav.classList.toggle('active');
-    icon.classList.toggle('open');
-    body.classList.toggle('no-scroll');
-}
