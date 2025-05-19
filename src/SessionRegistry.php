b96222691154ecf6b8eb14c01de9f3e34b197785
vinicius <cpudvini@gmail.com>
2025-05-19 11:53:28 -0300

    ajuste dnv

--- src/SessionRegistry.php
+++ src/SessionRegistry.php
@@ -6,7 +6,7 @@ namespace Vosouza\Sonomais;
 
 class SessionRegistry
 {
-    public static String $appVersion = "1.0.1";
+    public static String $appVersion = "1.0.2";
     private static ?array $session = null;
     private static String $isLoggedIn = "isloggedin";
     private static String $userId = "userid";
